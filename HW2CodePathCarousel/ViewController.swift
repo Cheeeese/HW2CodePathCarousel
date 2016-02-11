5e5d960b728098b7dd508373cb24474fee51af87
Matt Verghese <matt.verghese@gmail.com>
2016-02-11 02:05:31 -0800

    Initial Commit

---
+++ HW2CodePathCarousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW2CodePathCarousel
+//
+//  Created by Matthew Verghese on 2/11/16.
+//  Copyright (c) 2016 Cheeeese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
